2d63d378068e90f62bac944e577f43ee4970dc43
chilah <pasitaless@gmail.com>
2019-03-23 09:31:14 +0700

    rewrite code

--- pomodoro-clock.js
+++ pomodoro-clock.js
@@ -1,6 +1,6 @@
 const timer = {
-  sessionLength: 1,
-  breakLength: 0.07,
+  sessionLength: 0.1,
+  breakLength: 0.1,
   timeStart: 0,
   timeEnd: 0,
   timePause: 0,
@@ -13,65 +13,6 @@ const timer = {
 const startBtn = document.getElementById('start')
 const pauseBtn = document.getElementById('pause')
 const resumeBtn = document.getElementById('resume')
-let countdown;
-
-startBtn.addEventListener('click', function () {
-  startBtn.classList.add('hide')
-  pauseBtn.classList.remove('hide')
-
-  timer.isSession = true;
-  timer.timeStart = Date.now()
-  timer.timeSet = timer.sessionLength * 60 * 1000
-  timer.timeEnd = timer.timeStart + timer.timeSet
-
-  startTimer()
-})
-
-resumeBtn.addEventListener('click', function () {
-  pauseBtn.classList.remove('hide')
-  resumeBtn.classList.add('hide')
-
-  timer.timeStart = Date.now()
-  timer.timeEnd = timer.timeStart + timer.timePause
-
-  startTimer()
-})
-
-pauseBtn.addEventListener('click', function() {
-  resumeBtn.classList.remove('hide')
-  pauseBtn.classList.add('hide')
-
-  pauseTimer()
-})
-
-const startTimer = () => {
-  countdown = setInterval(function () {
-    const now = Date.now()
-    timer.timeRemaining = Math.floor(timer.timeEnd - now)
-
-    adjustTime(timer.timeRemaining)
-  }, 1000)
-}
-
-const pauseTimer = () => {
-  const pauseTime = Date.now()
-  timer.timePause = timer.timeEnd - pauseTime
-
-  clearInterval(countdown)
-}
-
-
-
-const adjustTime = (ms) => {
-  const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.round((ms % (1000 * 60)) / 1000)
-
-  const adjustMin = minutes < 10 ? `0${minutes}` : minutes
-  const adjustSec = seconds < 10 ? `0${seconds}` : seconds
-
-  displayTime.innerHTML = `${adjustMin}:${adjustSec}`
-  console.log(adjustMin, adjustSec)
-}
 
 // Display and Button section
 const displayTime = document.getElementById('displayTime');
