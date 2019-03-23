6a8853846f873da36388beaad640bacd7c2e94b5
chilah <pasitaless@gmail.com>
2019-03-23 14:57:34 +0700

    Add timer

--- pomodoro-clock.js
+++ pomodoro-clock.js
@@ -10,15 +10,47 @@ const timer = {
   isBreak: false
 }
 
-`from timer`
-`vbnmlkjdflg
-fl;jksldfjl
-we;ljrlwejr
-sdl;fk;sldjf
-4erpwoirp`
 const startBtn = document.getElementById('start')
 const pauseBtn = document.getElementById('pause')
 const resumeBtn = document.getElementById('resume')
+let countdown
+
+startBtn.addEventListener('click', function() {
+  timer.timeStart = timer.sessionLength * 60
+  pauseBtn.classList.remove('hide')
+  startBtn.classList.add('hide')
+  startTimer()
+})
+
+pauseBtn.addEventListener('click', function() {
+  clearInterval(countdown)
+  pauseBtn.classList.add('hide')
+  resumeBtn.classList.remove('hide')
+})
+
+resumeBtn.addEventListener('click', function() {
+  resumeBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+  startTimer()
+})
+const startTimer = () => {
+  countdown = setInterval(displayTimer, 1000)
+}
+
+const displayTimer = () => {
+  timer.timeStart--
+
+  let min = Math.floor(timer.timeStart / 60)
+  let sec = timer.timeStart % 60
+  
+  let adjustMin = min < 10 ? `0${min}` : min
+  let adjustSec = sec < 10 ? `0${sec}` : sec
+
+  if (timer.timeStart <= 0) {
+    clearInterval(countdown)
+  }
+  console.log(adjustMin, adjustSec)
+}
 
 // Display and Button section
 const displayTime = document.getElementById('displayTime');
